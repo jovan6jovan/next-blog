102ce9f7db643cf2196a927138f7f029feb26f0f
jovan6jovan <jovan6jovanovic@gmail.com>
2024-06-01 17:23:06 +0200

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { FC } from "react";
+
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <Head>
+        <title key="title">Next Blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+export default App;
